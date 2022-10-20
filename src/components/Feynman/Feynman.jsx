f0fa60e67d10ff6d303171d399e3537b27134065
auroraptor <interestingbutno@yandex.ru>
2022-10-20 21:48:05 +0300

    feat: add component

--- src/components/Feynman/Feynman.jsx
+++ src/components/Feynman/Feynman.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+// import { Link } from "react-router-dom";
 
 function Feynman() {
   return (
     <>
-    <div>533</div>
+        <h2 className="feynman__title">Метод Фейнмана</h2>
+        <p className="feynman__subtitle">Выучить и не забыть.</p>
+        {/* <Link className="feynman__link" href="#">Подробнее  &#8594;</Link> */}
     </>
   )
 }
